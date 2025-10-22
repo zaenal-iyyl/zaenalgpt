46e58a65d8f1b7c3328c947b9af101afd6c25cc3
Zaenalajj <clpmadang@gmail.com>
2025-10-22 10:06:54 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,20 @@
 
+import { useState, createContext, useContext } from "react";
+import "@/styles/globals.css";
+
+const ThemeContext = createContext();
+
+export const useTheme = () => useContext(ThemeContext);
+
+export default function App({ Component, pageProps }) {
+  const [dark, setDark] = useState(true);
+  const toggleTheme = () => setDark(!dark);
+
+  return (
+    <ThemeContext.Provider value={{ dark, toggleTheme }}>
+      <div className={dark ? "dark" : "light"}>
+        <Component {...pageProps} />
+      </div>
+    </ThemeContext.Provider>
+  );
+}
